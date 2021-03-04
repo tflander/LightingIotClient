86f9832d3c535bba69fb2d2125f4a0c5d649dc32
todd flanders <toddfbass@gmail.com>
2021-03-04 13:54:54 -0500

    added device-info component

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getPageText()).toEqual('Testing...1...2...3');
+    expect(await page.getPageText()).toContain('Testing...1...2...3');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceInfoComponent } from './device-info/device-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceInfoComponent
   ],
   imports: [
     BrowserModule,
