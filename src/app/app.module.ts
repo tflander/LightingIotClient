4cbedc5ff3518cc0c9be9f3149d57ce1d46011d5
todd flanders <toddfbass@gmail.com>
2021-03-05 11:33:43 -0500

    add messages for async stuffis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeviceInfoComponent } from './device-info/device-info.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeviceInfoComponent
+    DeviceInfoComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/device-info.service.ts
+++ src/app/device-info.service.ts
@@ -2,15 +2,18 @@ import {Injectable} from '@angular/core';
 import {DeviceInfo} from './device-info';
 import {DEVICES} from './MockDevices';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceInfoService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getDeviceInfo(): Observable<DeviceInfo[]> {
-    return of(DEVICES);
+    const observable = of(DEVICES);
+    this.messageService.add('DeviceInfoService: fetched devices');
+    return observable;
   }
 }
