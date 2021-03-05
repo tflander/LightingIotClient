3772463c885d2e4fe01f0e29dcb5fe52a42aa8df
todd flanders <toddfbass@gmail.com>
2021-03-05 11:00:16 -0500

    wire the device info service to the device info component

--- src/app/device-info/device-info.component.ts
+++ src/app/device-info/device-info.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceInfo} from '../device-info';
 import { DEVICES } from '../MockDevices';
+import { DeviceInfoService} from '../device-info.service';
 
 @Component({
   selector: 'app-device-info',
   templateUrl: './device-info.component.html',
   styleUrls: ['./device-info.component.sass']
 })
+
 export class DeviceInfoComponent implements OnInit {
 
-  mockDevices = DEVICES;
+  devices: DeviceInfo[] = [];
 
-  constructor() { }
+  getDevices(): void {
+    this.devices = this.deviceInfoService.getDeviceInfo();
+  }
+  constructor(private deviceInfoService: DeviceInfoService) { }
   ngOnInit(): void {
+    this.getDevices();
   }
 
 }
