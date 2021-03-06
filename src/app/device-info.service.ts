f72dd86658475d910561e40f197e9b2fe045cb80
todd flanders <toddfbass@gmail.com>
2021-03-05 20:54:20 -0500

    first pass at error handling

--- src/app/device-info.service.ts
+++ src/app/device-info.service.ts
@@ -4,13 +4,14 @@ import {DEVICES} from './MockDevices';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceInfoService {
 
-  private deviceUrl = '/api/info';
+  private deviceUrl = '/api5/info';
 
   constructor(
     private messageService: MessageService,
@@ -19,11 +20,30 @@ export class DeviceInfoService {
 
   // TODO: handle multiple devices, e.g. Observable<DeviceInfo[]>
   getDeviceInfo(): Observable<DeviceInfo> {
-    // const observable = of(DEVICES);
-    const observable = this.http.get<DeviceInfo>(this.deviceUrl);
-    this.messageService.add('DeviceInfoService: fetched devices');
-    console.log(observable);
-    return observable;
+
+    // Return mocks
+    // return of(DEVICES);
+
+    return this.http.get<DeviceInfo>(this.deviceUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<DeviceInfo>('getHeroes', undefined))
+      );
+  }
+
+  // tslint:disable-next-line:typedef
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      // console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   private log(message: string) {
