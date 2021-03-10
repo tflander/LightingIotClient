6a33d08aeaa491b36ca9822b95e965b689408a64
todd flanders <toddfbass@gmail.com>
2021-03-10 10:05:27 -0500

    scale 10 bit colors to 8 bit

--- src/app/rgbw-color-selector/rgbw-color-selector.component.ts
+++ src/app/rgbw-color-selector/rgbw-color-selector.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { ColorPickerService, Cmyk } from 'ngx-color-picker';
 import { DeviceInfo} from '../device-info';
+import {ColorDuties} from '../colorDuties';
 
 @Component({
   selector: 'app-rgbw-color-selector',
@@ -13,6 +14,7 @@ export class RgbwColorSelectorComponent implements OnInit {
   device!: DeviceInfo;
 
   public color1 = '#2889e9';  // TODO: set to current color
+  public scaledColors: ColorDuties;
 
   // TODO: event processing per
   //   https://stackblitz.com/github/zefoy/ngx-color-picker/tree/master?file=projects%2Fapp%2Fsrc%2Fapp%2Fapp.component.ts
@@ -21,9 +23,25 @@ export class RgbwColorSelectorComponent implements OnInit {
     console.log(event, data);
   }
 
-  constructor(private cpService: ColorPickerService) { }
+  constructor(private cpService: ColorPickerService) {
+    this.scaledColors = new class implements ColorDuties {
+      Red!: number;
+      Green!: number;
+      Blue!: number;
+      White!: number;
+      UltraViolet!: number;
+    }();
+
+  }
 
   ngOnInit(): void {
+    if (this.device.duties) {
+      this.scaledColors.Red = Math.floor(this.device.duties.Red / 4);
+      this.scaledColors.Green = Math.floor(this.device.duties.Green / 4);
+      this.scaledColors.Blue = Math.floor(this.device.duties.Blue / 4);
+      this.scaledColors.White = Math.floor(this.device.duties.White / 4);
+      this.scaledColors.UltraViolet = Math.floor(this.device.duties.UltraViolet / 4);
+    }
   }
 
 }
