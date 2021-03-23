2b8a5abd6d36a844ab22d98afc2c217c030292b6
todd flanders <toddfbass@gmail.com>
2021-03-23 08:21:05 -0400

    calculate white duties

--- src/app/rgb-to-duties.service.ts
+++ src/app/rgb-to-duties.service.ts
@@ -16,18 +16,16 @@ export class RgbToDutiesService {
     const hsva = this.cpService.stringToHsva(rgb);
     if (hsva) {
       const rgba = this.cpService.hsvaToRgba(hsva);
-      console.log(rgba);
       duties.Red = Math.round(rgba.r * 1023);
       duties.Green = Math.round(rgba.g * 1023);
       duties.Blue = Math.round(rgba.b * 1023);
 
-      /*
-      if (rgb === '#ffffff') {
-        duties.White = 1023;
-      }
-       */
+      const white = Math.min(duties.Red, duties.Green, duties.Blue);
+      duties.White = white;
+      duties.Red -= white;
+      duties.Green -= white;
+      duties.Blue -= white;
     }
-
     return duties;
   }
 }
